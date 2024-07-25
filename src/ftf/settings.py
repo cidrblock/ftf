816b103c427d80b07d52f2f9ba6472ae4ed547c8
Bradley A. Thornton <bthornto@redhat.com>
2024-07-24 19:58:46 -0700

    Add soanr

--- src/ftf/settings.py
+++ src/ftf/settings.py
@@ -1,6 +1,10 @@
 """Some settings in python format to avoid missing types from a yaml or toml file."""
 
 REPOS: dict[str, dict[str, str]] = {
+    "ansible-compat": {
+        "origin": "{origin_org}/ansible-compat",
+        "upstream": "ansible/ansible-compat",
+    },
     "ansible-creator": {
         "origin": "{origin_org}/ansible-creator",
         "upstream": "ansible/ansible-creator",
@@ -12,6 +16,14 @@ REPOS: dict[str, dict[str, str]] = {
     "ansible-dev-tools": {
         "origin": "{origin_org}/ansible-dev-tools",
         "upstream": "ansible/ansible-dev-tools",
+    },
+    "ansible-lint": {
+        "origin": "{origin_org}/ansible-lint",
+        "upstream": "ansible/ansible-lint",
+    },
+    "ansible-navigator": {
+        "origin": "{origin_org}/ansible-navigator",
+        "upstream": "ansible/ansible-navigator",
     },
     "molecule": {
         "origin": "{origin_org}/molecule",
@@ -30,19 +42,44 @@ REPOS: dict[str, dict[str, str]] = {
 FULL_FILES: dict[str, dict[str, list[str]]] = {
     ".github/CODE_OF_CONDUCT.md": {},
     ".github/CODEOWNERS": {},
-    ".github/dependabot.yml": {"skip": ["ansible-dev-tools"]},
-    ".github/release-drafter.yml": {"skip": ["ansible-dev-tools"]},
-    ".github/workflows/ack.yml": {},
-    ".github/workflows/push.yml": {},
-    ".github/workflows/release.yml": {"skip": ["ansible-dev-tools"]},
-    ".github/workflows/tox.yml": {"skip": ["molecule", "ansible-dev-tools"]},
-    ".readthedocs.yml": {},
-    ".vscode/extensions.json": {},
-    ".vscode/settings.json": {"skip": ["ansible-dev-tools"]},
-    ".vscode/tasks.json": {},
-    "codecov.yml": {},
-    "__cspell.config.yaml": {},
-    "tox.ini": {"skip": ["ansible-dev-tools", "molecule"]},
+    ".github/dependabot.yml": {
+        "skip": ["ansible-dev-tools", "ansible-compat", "ansible-lint", "ansible-navigator"],
+    },
+    ".github/release-drafter.yml": {
+        "skip": ["ansible-dev-tools", "ansible-compat", "ansible-lint", "ansible-navigator"],
+    },
+    ".github/workflows/ack.yml": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    ".github/workflows/push.yml": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    ".github/workflows/release.yml": {
+        "skip": ["ansible-dev-tools", "ansible-compat", "ansible-lint", "ansible-navigator"],
+    },
+    ".github/workflows/tox.yml": {
+        "skip": [
+            "molecule",
+            "ansible-dev-tools",
+            "ansible-compat",
+            "ansible-lint",
+            "ansible-navigator",
+        ],
+    },
+    ".readthedocs.yml": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    ".sonarcloud.properties": {"skip": ["ansible-compat", "ansible-lint"]},
+    ".vscode/extensions.json": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    ".vscode/settings.json": {
+        "skip": ["ansible-dev-tools", "ansible-compat", "ansible-lint", "ansible-navigator"],
+    },
+    ".vscode/tasks.json": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    "codecov.yml": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    "__cspell.config.yaml": {"skip": ["ansible-compat", "ansible-lint", "ansible-navigator"]},
+    "tox.ini": {
+        "skip": [
+            "ansible-dev-tools",
+            "molecule",
+            "ansible-compat",
+            "ansible-lint",
+            "ansible-navigator",
+        ],
+    },
 }
 
 SORT_LOWER: list[str] = [
